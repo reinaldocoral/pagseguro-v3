6196192f011e1454cd66a08f89d963c1665af0d0
Reinaldo Coral <morfeu.rio@gmail.com>
2023-10-04 16:58:06 -0300

    Initial commit

---
+++ src/Checkout/PagamentoRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ReinaldoCoral\Pagseguro\Checkout;
+
+use GuzzleHttp\Client;
+use ReinaldoCoral\Pagseguro\Configure;
+
+class PagamentoRequest
+{
+    private $payload;
+
+    public function __construct()
+    {
+        
+    }
+
+    public function setPayload( $payload )
+    {
+        $this->payload = $payload;
+    }
+
+    public function execute(Configure $config)
+    {
+        $http_client = new Client(['base_uri' => $config->getEndpointBase()]);
+        $response = $http_client->request('POST', '/checkouts', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $config->getAccountToken(),
+                'Content-Type' => 'application/json',
+                'x-idempotency-key' => ''
+            ],
+            'body'    => json_encode($this->payload),
+        ]);
+
+        return $response;
+    }
+}
---
+++ src/Configure.php
@@ -0,0 +1,43 @@
+<?php 
+
+namespace ReinaldoCoral\Pagseguro;
+
+
+class Configure
+{
+    private $email;
+    private $token;
+    private $environment;
+
+    public function __construct()
+    {
+        
+    }
+
+    public function setAccountCredentials( $email, $token )
+    {
+        $this->email = $email;
+        $this->token = $token;
+    }
+
+    public function setEnvironment( $environment = 'sandbox' )
+    {
+        $this->environment = $environment;
+    }
+
+    public function getAccountToken()
+    {
+        return $this->token;
+    }
+
+    public function getEndpointBase()
+    {
+        return $this->environment === 'production' ? 'https://api.pagseguro.com' : 'https://sandbox.api.pagseguro.com';
+    }
+
+    public function getSecureEndpointBase()
+    {
+        return $this->environment === 'production' ? 'https://secure.api.pagseguro.com' : 'https://secure.sandbox.api.pagseguro.com';
+    }
+
+}
